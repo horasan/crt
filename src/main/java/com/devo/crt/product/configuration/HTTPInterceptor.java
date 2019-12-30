f0cadcc7170d2c4078fde3fa7005221f7d429777
Horasan, Riza <rizahorasan@gmail.com>
2019-12-30 23:15:22 +0000

    HTTP interceptor is implemented.

---
+++ src/main/java/com/devo/crt/product/configuration/HTTPInterceptor.java
@@ -0,0 +1,29 @@
+package com.devo.crt.product.configuration;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class HTTPInterceptor extends HandlerInterceptorAdapter {
+	@Override
+    public boolean preHandle(HttpServletRequest requestServlet, HttpServletResponse responseServlet, Object handler) throws Exception
+    {
+        System.out.println("HTTP INTERCEPTOR PREHANDLE CALLED");
+
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception
+    {
+        System.out.println("HTTP INTERCEPTOR POSTHANDLE CALLED");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception exception) throws Exception
+    {
+        System.out.println("HTTP INTERCEPTOR AFTERCOMPLETION CALLED");
+    }
+}
